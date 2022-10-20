5c7d57c1262b22b3bdd0e283bb4edb2647d51f9a
Káthia Rocha <kathia.karine2015@gmail.com>
2022-10-20 13:11:07 -0300

    Correção de bugs: 1 - Posição da cabeça da cobra ao iniciar. 2 - Congelamento da cobra ao atravessar a parede lateral.

--- jogoCobrinha.2.0.cpp
+++ jogoCobrinha.2.0.cpp
@@ -10,16 +10,27 @@ int quantLinhasTab = 20;
 int quantColunasTab = 30;
 char tabuleiro[20][30];
 int cobra[8][2];
-int aux[2];
-int colunaFruta=3;
-int linhaFruta=2;
-int x,y,i,pontos,tamCobra=3;
+int controlePosiCabeca[2];
+int colunaFruta = 3;
+int linhaFruta = 2;
+int x,y,i,pontos,tamCobra = 3;
 char tCIMA = 119;
 char tBAIXO = 115;
 char tDIREITA = 100;
 char tESQUERDA = 97;
 char tecla;
 
+inicializarCobra(){
+	
+	cobra[0][0] = 6;
+	cobra[1][0] = 5;
+	cobra[1][1] = 6;
+	cobra[2][0] = 4;
+	cobra[2][1] = 6;
+	cobra[0][1] = 6;
+ 	controlePosiCabeca[0] = cobra[0][0];
+ 	controlePosiCabeca[1] = cobra[0][1];
+}
 
 gerarNovaFruta(){
 	
@@ -37,71 +48,71 @@ capturarTecla(){
 		teclaPress = getch();
 	}
 	
-	if(teclaPress==tCIMA ||teclaPress==tBAIXO ||teclaPress==tDIREITA ||teclaPress==tESQUERDA){
-		tecla=teclaPress;
+	if(teclaPress == tCIMA || teclaPress == tBAIXO || teclaPress == tDIREITA || teclaPress == tESQUERDA){
+		tecla = teclaPress;
 	}
 }
 
 moverCobra(){
 	
-	i=tamCobra;
+	i = tamCobra;
 
 	//comando w no teclado
 	if(tecla == tCIMA){
 		if(cobra[0][1] > 1){
-			aux[1]= cobra[0][1]-1;
+			controlePosiCabeca[1] = cobra[0][1] - 1;
 		}
 		else{
-			aux[1]= quantLinhasTab-2;
+			controlePosiCabeca[1] = quantLinhasTab - 2;
 		}
-		aux[0]= cobra[0][0];	
+		controlePosiCabeca[0] = cobra[0][0];	
 	}
 	
 	//comando s no teclado
 	if(tecla == tBAIXO){
 		if(cobra[0][1] < quantLinhasTab - 2){
-			aux[1]= cobra[0][1] + 1;
+			controlePosiCabeca[1] = cobra[0][1] + 1;
 		}
 		else{
-			aux[1]= 1;
+			controlePosiCabeca[1] = 1;
 		}
-		aux[0]= cobra[0][0];
+		controlePosiCabeca[0] = cobra[0][0];
 	}
 	
 	//comando a no teclado
 	if(tecla == tESQUERDA){
 		if(cobra[0][0] > 1){
-			aux[0] = cobra[0][0] - 1;
+			controlePosiCabeca[0] = cobra[0][0] - 1;
 		}
 		else{
-			cobra[0][0] = quantColunasTab - 2;
+			controlePosiCabeca[0] = quantColunasTab - 2;
 		}
-		aux[1]= cobra[0][1];
+		controlePosiCabeca[1] = cobra[0][1];
 	}
 	
 	//comando d no teclado
 	if(tecla == tDIREITA){
 		if(cobra[0][0] < quantColunasTab - 2){
-			aux[0] = cobra[0][0]+1;
+			controlePosiCabeca[0] = cobra[0][0] + 1;
 		}
 		else{
-			cobra[0][0] = 1;
+			controlePosiCabeca[0] = 1;
 		}
-		aux[1]= cobra[0][1];
+		controlePosiCabeca[1] = cobra[0][1];
 	}
 	
 		
 	for(i;i>1;i--){
-		cobra[i-1][0]=cobra[i-2][0];
-		cobra[i-1][1]=cobra[i-2][1];
+		cobra[i-1][0] = cobra[i-2][0];
+		cobra[i-1][1] = cobra[i-2][1];
 	}
 	
-	cobra[0][0]=aux[0];
-	cobra[0][1]=aux[1];
+	cobra[0][0] = controlePosiCabeca[0];
+	cobra[0][1] = controlePosiCabeca[1];
 }
 
 verificarSeFrutaFoiComida(){
-	if(cobra[0][0]==colunaFruta && cobra[0][1]==linhaFruta){
+	if(cobra[0][0] == colunaFruta && cobra[0][1] == linhaFruta){
 		pontos = pontos + 1;
 		tamCobra = tamCobra + 1;
 		gerarNovaFruta();
@@ -109,32 +120,25 @@ verificarSeFrutaFoiComida(){
 }
 
 gerarTabuleiro(){
-	
-	//int x,y,yt,xt;
-	
-
 	for (y = 0; y < quantLinhasTab; y++){
 		for (x = 0; x < quantColunasTab; x++){
-			if( y == 0 || y == quantLinhasTab-1 || x==0 || x==quantColunasTab-1){
-				tabuleiro[y][x]=177;
+			if( y == 0 || y == quantLinhasTab - 1 || x == 0 || x == quantColunasTab - 1){
+				tabuleiro[y][x] = 177;
 			}
-			else if(y==linhaFruta && x==colunaFruta){
-				tabuleiro[y][x]=208;
-				
+			else if(y == linhaFruta && x == colunaFruta){
+				tabuleiro[y][x] = 208;
 			}
 			else{
-				tabuleiro[y][x]=' ';
+				tabuleiro[y][x] = ' ';
 			}
 		}
 	}
 	
-	for(i=0;i<tamCobra;i++){
-		y=cobra[i][1];
-		x=cobra[i][0];
-		
-		tabuleiro [y][x]=220;
+	for(i = 0; i < tamCobra; i++){
+		y = cobra[i][1];
+		x = cobra[i][0];
+		tabuleiro [y][x] = 220;
 	}
-	
 }
 
 
@@ -151,30 +155,27 @@ imprimirTabuleiro(){
 	}
 }
 
-int main(){
+iniciarJogo(){
 	
-	cobra[0][0] = 6;
-	cobra[0][1] = 6;
-	cobra[1][0] = 5;
-	cobra[1][1] = 6;
-	cobra[2][0] = 4;
-	cobra[2][1] = 6;
-
+	inicializarCobra();
 	
-		
 	while (1){
-		
 		capturarTecla();
 		moverCobra();
 		verificarSeFrutaFoiComida();
 		gerarTabuleiro();
 		imprimirTabuleiro();
 		sleep(0.5);
-
 	}
+}
+
+int main(){
 	
-	//gerarNovaFruta();
-	//printf("%d , %d ", colunaFruta, linhaFruta);
+	
+		
+	iniciarJogo();
+	
+	
 	
 	
 	
